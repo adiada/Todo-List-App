ab84e14a6cd31ca05e4685f89f11a1e44f48566b
Adharsh Suresh <adharshsuresh1997@gmail.com>
2021-11-16 01:34:43 +0530

    Added error handling page, Added Mongoose, Added fetch for deletion instead of AJAX

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,6 @@
 let data = [{item:'get milk'},{item:"walk dog"},{item:"kick some coding ass"}]
 let express = require('express')
+let mongoose = require('mongoose')
 // let bodyParser = require('body-parser')
 // let urlencodedParser = bodyParser.urlencoded({extended:false})
 // let jsonParser = bodyParser.json({type:"*/*"});
@@ -19,8 +20,8 @@ module.exports = function(app){
         data.push(req.body)
         // console.log('items in list')
         // data.forEach((item) => {console.log(JSON.stringify(item))})
-        // res.json(data)
-        res.redirect('/todo');
+        res.json(data)
+        // res.redirect('/todo');
     })
 
 
@@ -28,5 +29,24 @@ module.exports = function(app){
         // console.log(req.params.item)
         data = data.filter((todo)=> todo.item.replace(/ /g,'-') !== req.params.item)
         res.json(data)
+    })
+
+
+    app.use(function(req,res,next){
+        
+        res.status(404);
+
+        if(req.accepts('html')){
+            res.render('404',{url:req.url});
+            return;
+        }
+
+        if(req.accepts('json')){
+            res.json({error : 'Not found'});
+            return;
+        }
+
+        res.type('txt').send('Not found');
+
     })
 };
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -29,7 +29,6 @@ myForm.addEventListener('submit',async function(e){
     e.preventDefault();
 
     const formData = new FormData(this);
-    alert(formData.entries())
     const formDataSerialized = Object.fromEntries(formData);
 
     fetch('/todo', {
@@ -37,23 +36,49 @@ myForm.addEventListener('submit',async function(e){
             method:'post',
             body:JSON.stringify(formDataSerialized), 
     })
-    .then( (resp) => {  return resp.text()})
+    .then( (resp) => { location.reload(); return resp.text()})
     .then( txtData => console.log(txtData))
     .catch( e => console.log('error',e.message));
 
 });
 
 
-  $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
-      $.ajax({
-        type: 'DELETE',
-        url: '/todo/' + item,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
-  });
+
+
+const liElements = document.getElementsByTagName('LI');
+
+//we cannot use forEach for liElements as it is an HTML collection and not an array
+
+for( let i = 0 ; i < liElements.length ; i++){
+ liElements[i].addEventListener('click',async function(e){
+      let item = this.innerText.replace(/ /g,'-');
+      // console.log('got clicked');
+      try{
+
+        const response = await fetch('/todo/'+item, 
+                                      {
+                                        method:'delete',
+                                      });
+        responseAsData = await response.json();
+        console.log(responseAsData);
+        location.reload();
+      }catch(e){
+        console.log('Error : ',e.message);
+      }
+});
+}
+
+
+  // $('li').on('click', function(){
+  //     var item = $(this).text().replace(/ /g, "-");
+  //     $.ajax({
+  //       type: 'DELETE',
+  //       url: '/todo/' + item,
+  //       success: function(data){
+  //         //do something with the data via front-end framework
+  //         location.reload();
+  //       }
+  //     });
+  // });
 
 // });
